6644b7f2c21d3dbb912a6fe020dca50a1097a76d
shubham <bhattshubham15@gmail.com>
2019-05-03 18:57:27 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/departments', 'DepartmentsController@index')
+// ->name('departmentLists');
+//->middleware('test');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
